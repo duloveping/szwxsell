7823ec9305c48cb4dff8e3e7d7718646c76e2dad
duyongsheng <duloveping@qq.com>
2019-11-07 00:26:51 +0800

    添加会员联系人方式类型的常量

--- src/main/java/cn/net/szwx/sell/common/Constants.java
+++ src/main/java/cn/net/szwx/sell/common/Constants.java
@@ -12,7 +12,32 @@ public class Constants {
     public static final Integer YES = 2;
     public static final Integer NO = 3;
 
+    /** 会员锁定状态 - 启动 */
     public static final Integer ACCOUNT_LOCK_STATE_ENABLED = 1;
+    /** 会员锁定状态 - 禁用 */
     public static final Integer ACCOUNT_LOCK_STATE_DISABLED = 2;
+
+    /** 联系人方式类型 - 其他人 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_OTHER = 0;
+    /** 联系人方式类型 - 本人 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_SELF = 1;
+    /** 联系人方式类型 - 父母 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_PARENT = 2;
+    /** 联系人方式类型 - 亲属 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_RELATIVE = 3;
+    /** 联系人方式类型 - 同事 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_WORKMATE = 4;
+    /** 联系人方式类型 - 同学 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_CLASSMATE = 5;
+    /** 联系人方式类型 - 朋友 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_FRIEND = 6;
+    /** 联系人方式类型 - 兄弟 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_BROTHER = 7;
+    /** 联系人方式类型 - 姐妹 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_SISTER = 8;
+    /** 联系人方式类型 - 子女 */
+    public static final Integer MEMBER_CONTACT_CATEGORY_CHILDREN = 9;
+
+
 
 }
--- src/main/resources/static/templates/common/constants.js
+++ src/main/resources/static/templates/common/constants.js
@@ -3,3 +3,29 @@ const RESULT_STATUS_FAILURE = 1;
 
 const ACCOUNT_LOCK_STATE_ENABLED = 1;
 const ACCOUNT_LOCK_STATE_DISABLED = 2;
+
+/** 会员锁定状态 - 启动 */
+const ACCOUNT_LOCK_STATE_ENABLED = 1;
+/** 会员锁定状态 - 禁用 */
+const ACCOUNT_LOCK_STATE_DISABLED = 2;
+
+/** 联系人方式类型 - 其他人 */
+const MEMBER_CONTACT_CATEGORY_OTHER = 0;
+/** 联系人方式类型 - 本人 */
+const MEMBER_CONTACT_CATEGORY_SELF = 1;
+/** 联系人方式类型 - 父母 */
+const MEMBER_CONTACT_CATEGORY_PARENT = 2;
+/** 联系人方式类型 - 亲属 */
+const MEMBER_CONTACT_CATEGORY_RELATIVE = 3;
+/** 联系人方式类型 - 同事 */
+const MEMBER_CONTACT_CATEGORY_WORKMATE = 4;
+/** 联系人方式类型 - 同学 */
+const MEMBER_CONTACT_CATEGORY_CLASSMATE = 5;
+/** 联系人方式类型 - 朋友 */
+const MEMBER_CONTACT_CATEGORY_FRIEND = 6;
+/** 联系人方式类型 - 兄弟 */
+const MEMBER_CONTACT_CATEGORY_BROTHER = 7;
+/** 联系人方式类型 - 姐妹 */
+const MEMBER_CONTACT_CATEGORY_SISTER = 8;
+/** 联系人方式类型 - 子女 */
+const MEMBER_CONTACT_CATEGORY_CHILDREN = 9;
